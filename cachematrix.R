a9146300f98cc176006f583053f33c6423dab522
DanielJR <danieljosefo@uwalumni.com>
2018-07-03 11:24:23 -0500

    Populated functions makeCacheMatrix and cacheSolve. Added descriptive comments for each. DJR 07-03-18

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are used to calculate the 
+## inverse of a matrix and cache it.
+## The cached inverse can be recovered
+## without incurring in repeat computation.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+
+## creates a list containing a function to
+## 1) set the content of matrix
+## 2) get the contentof the matrix
+## 3) set the content of the inverse matrix
+## 3) get the content of the inverse matrix
+
+makeCacheMatrix <- function(X = numeric()) {
+        Xinv <- NULL
+        set <- function(Y) {
+                X <<- Y
+                Xinv <<- NULL
+                
+        }
+        get <-function() X ##may need to parse as vector
+        setinv <- function(solve) Xinv <<- solve ##may need to parse as vector
+        getinv <- function() Xinv
+        list (set = set, get = get,
+              setinv = setinv, getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## calculates the inverse of the special 
+## matrix created with the above function
+## or recovers the existing cached inverse.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(X,...) {
+        
+        ## Return a matrix that is the inverse of 'X'
+        
+        Xinv <- X$getinv()
+        if (!is.null(Xinv)) {
+                message("getting cached inverse")
+                return(Xinv)
+        }
+        data <- X$get()
+        Xinv <- solve(data,...)
+        X$setinv(Xinv)
+        Xinv
+        
 }
